c377afe1a96d3adb135e146cce293730fe2a4ec8
Parker J. Rule <parker.rule@tufts.edu>
2023-01-27 15:38:11 -0500

    Add utility script for database regen

---
+++ nuke.py
@@ -0,0 +1,30 @@
+"""Refresh DB."""
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from cherrydb_meta.admin import CherryAdmin
+from cherrydb_meta.models import Base
+
+NAME = "Parker Rule"
+EMAIL = "pjrule@me.com"
+
+if __name__ == "__main__":
+    engine = create_engine(os.getenv("CHERRY_DATABASE_URI"))
+    with engine.connect() as conn:
+        conn.execute("DROP SCHEMA cherrydb CASCADE")
+        conn.execute("CREATE SCHEMA cherrydb")
+    Base.metadata.create_all(engine)
+
+    db = sessionmaker(engine)()
+    admin = CherryAdmin(session=db)
+    user = admin.user_create(name=NAME, email=EMAIL)
+    api_key = admin.key_create(user=user)
+    db.commit()
+    db.close()
+
+    print(api_key)
+    with open(".cherryrc", "w") as fp:
+        print(f'export CHERRY_USER="{NAME}"', file=fp)
+        print(f'export CHERRY_API_KEY="{api_key}"', file=fp)
