9f8973ce4eac2c5a9236e93b4447c20210b6e6d0
Parker J. Rule <parker.rule@tufts.edu>
2023-03-13 21:24:45 -0400

    Tweak environment variables in DB config

--- cherrydb_meta/db.py
+++ cherrydb_meta/db.py
@@ -5,8 +5,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-print("params:", os.getenv("INSTANCE_UNIX_SOCKET"), os.getenv("DB_USER"))
-
 if os.getenv("DB"):
     # AWS App Runner deployment: parse JSON blob with credentials.
     creds = json.loads(os.getenv("DB"))
@@ -14,11 +12,11 @@ if os.getenv("DB"):
         f'postgresql://{creds["username"]}:{creds["password"]}'
         f'@{creds["host"]}:{creds["port"]}/{creds["dbname"]}'
     )
-elif os.getenv("INSTANCE_UNIX_SOCKET"):
+elif os.getenv("INSTANCE_CONNECTION_NAME"):
     username = os.environ["DB_USER"]
     password = os.environ["DB_PASS"]
     db_name = os.environ["DB_NAME"]
-    socket = os.environ["INSTANCE_UNIX_SOCKET"]
+    socket = f'/cloudsql/{os.environ["INSTANCE_CONNECTION_NAME"]}'
     db_url = (
         f"postgresql://{username}:{password}@/{db_name}"
         f"?unix_sock={socket}/.s.PGSQL.5432"
