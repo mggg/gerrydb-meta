abda68b8785a5fc50d4776947dad625007d31d0f
peterrrock2 <peter.r.rock2@gmail.com>
2024-04-02 13:00:56 -0600

    Add some middleware to deal with 422 errors

--- gerrydb_meta/main.py
+++ gerrydb_meta/main.py
@@ -1,7 +1,8 @@
 """Entrypoint for Gerry API server."""
 from http import HTTPStatus
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from starlette.responses import StreamingResponse
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import JSONResponse
 
@@ -13,6 +14,8 @@ from gerrydb_meta.exceptions import (
     CreateValueError,
 )
 
+import logging
+
 API_PREFIX = "/api/v1"
 
 app = FastAPI(title="gerrydb-meta", openapi_url=f"{API_PREFIX}/openapi.json")
@@ -23,7 +26,9 @@ def create_value_error(request: Request, exc: CreateValueError):
     """Handles generic object creation failures."""
     return JSONResponse(
         status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-        content={"detail": f"Object creation failed. Reason: {exc}"},
+        content={
+            "kind": "Create value error",
+            "detail": f"Object creation failed. Reason: {exc}"},
     )
 
 
@@ -32,7 +37,10 @@ def column_value_type_error(request: Request, exc: ColumnValueTypeError):
     """Handles generic object creation failures."""
     return JSONResponse(
         status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-        content={"detail": "Type errors found in column values.", "errors": exc.errors},
+        content={
+            "kind": "Column value type error",
+            "detail": "Type errors found in column values.", 
+            "errors": exc.errors},
     )
 
 
@@ -42,6 +50,7 @@ def bulk_create_error(request: Request, exc: BulkCreateError):
     return JSONResponse(
         status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
         content={
+            "kind": "Bulk create error",
             "detail": f"Object creation failed. Reason: {exc}",
             "paths": exc.paths,
         },
@@ -54,6 +63,7 @@ def bulk_create_error(request: Request, exc: BulkCreateError):
     return JSONResponse(
         status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
         content={
+            "kind": "BulkPatchError",
             "detail": f"Object patch failed. Reason: {exc}",
             "paths": exc.paths,
         },
@@ -63,6 +73,44 @@ def bulk_create_error(request: Request, exc: BulkCreateError):
 app.add_middleware(GZipMiddleware)
 app.include_router(api_router, prefix=API_PREFIX)
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+MAX_BUFFER_SIZE = 1024
+
+@app.middleware("http")
+async def log_422_errors(request: Request, call_next):
+    response = await call_next(request)
+    
+    # Check for the specific status code
+    if response.status_code == 422:
+        print(response.headers)
+        
+        # If the response is a StreamingRessponse, you need to iterate over the body
+        # and consume it to log the content
+        if isinstance(response, StreamingResponse):
+            body_content = b''
+            async for chunk in response.body_iterator:
+                body_content += chunk
+                if len(body_content) > MAX_BUFFER_SIZE:
+                    body_content = body_content[:MAX_BUFFER_SIZE]
+                    break
+            logger.info(
+                f"\n\t422 for Request: {request.method},\n\tAt: {request.url}\n\tBody: {body_content.decode()}"
+            )
+            return Response(content=body_content, status_code=422, headers=dict(response.headers))
+        else:
+            # For non-streaming responses, you can directly access the body
+            body_content = b''
+            if hasattr(response, 'body'):
+                body_content = response.body
+            logger.info(
+                f"\n\t422 for Request: {request.method},\n\tAt: {request.url}\n\tBody: {body_content.decode()}"
+            )
+    
+    return response
 
 @app.get("/health")
 def health_check():
